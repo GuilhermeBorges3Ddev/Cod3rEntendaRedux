9e86941f0bb6a1719640d1d3d8d67c20f6dce99f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-02-12 12:08:26 -0300

    Basilar components Sidebar and Video mounted on UI

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,21 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+import Sidebar from './components/Sidebar';
+import Video from './components/Video';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Sidebar />
+        <Video />
+      </div>
+    )
+  }
 }
+
 
 export default App;
